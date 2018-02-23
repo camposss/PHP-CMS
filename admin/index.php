ef1b88fa97bbd30b3aee35932a8613e1a0fc3cad
camposss <ccamposs23@gmail.com>
2018-02-23 07:59:14 -0800

    Added widgets and dynamic chart to dashboard page

--- admin/index.php
+++ admin/index.php
@@ -4,9 +4,7 @@
 
         <? include "admin_includes/admin_nav.php" ?>
         <div id="page-wrapper">
-
             <div class="container-fluid">
-
                 <!-- Page Heading -->
                 <div class="row"></div>
                     <div class="col-lg-12">
@@ -15,15 +13,179 @@
                         <?php echo $_SESSION['firstname'] ?>
                             <small>You are the admin</small>
                         </h1>
-            
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-3 col-md-6">
+                        <div class="panel panel-primary">
+                            <div class="panel-heading">
+                                <div class="row">
+                                    <div class="col-xs-3">
+                                        <i class="fa fa-file-text fa-5x"></i>
+                                    </div>
+                                    <div class="col-xs-9 text-right">
+                                
+                <?php
+                    $query= "SELECT * FROM posts";
+                    $select_all_posts= mysqli_query($conn, $query);
+                    $post_counts= mysqli_num_rows($select_all_posts);
+                    echo "<div class='huge'>{$post_counts}</div>"
+
+                ?>
+                                        <div>Posts</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <a href="posts.php">
+                                <div class="panel-footer">
+                                    <span class="pull-left">View Details</span>
+                                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                    <div class="clearfix"></div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6">
+                        <div class="panel panel-green">
+                            <div class="panel-heading">
+                                <div class="row">
+                                    <div class="col-xs-3">
+                                        <i class="fa fa-comments fa-5x"></i>
+                                    </div>
+                                    <div class="col-xs-9 text-right">                     
+                <?php
+                    $query= "SELECT * FROM comments";
+                    $select_all_comments= mysqli_query($conn, $query);
+                    $comment_counts= mysqli_num_rows($select_all_comments);
+                    echo "<div class='huge'>{$comment_counts}</div>"
+                ?>
+                                    <div>Comments</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <a href="comments.php">
+                                <div class="panel-footer">
+                                    <span class="pull-left">View Details</span>
+                                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                    <div class="clearfix"></div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6">
+                        <div class="panel panel-yellow">
+                            <div class="panel-heading">
+                                <div class="row">
+                                    <div class="col-xs-3">
+                                        <i class="fa fa-user fa-5x"></i>
+                                    </div>
+                                    <div class="col-xs-9 text-right">
+                <?php
+                    $query= "SELECT * FROM users";
+                    $select_all_users= mysqli_query($conn, $query);
+                    $user_counts= mysqli_num_rows($select_all_users);
+                    echo "<div class='huge'>{$user_counts}</div>"
+                ?>                  
+                                        <div> Users</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <a href="users.php">
+                                <div class="panel-footer">
+                                    <span class="pull-left">View Details</span>
+                                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                    <div class="clearfix"></div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6">
+                        <div class="panel panel-red">
+                            <div class="panel-heading">
+                                <div class="row">
+                                    <div class="col-xs-3">
+                                        <i class="fa fa-list fa-5x"></i>
+                                    </div>
+                                    <div class="col-xs-9 text-right">
+                <?php
+                    $query= "SELECT * FROM categories";
+                    $select_all_categories= mysqli_query($conn, $query);
+                    $categories_counts= mysqli_num_rows($select_all_categories);
+                    echo "<div class='huge'>{$categories_counts}</div>"
+                ?>    
+                                        <div>Categories</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <a href="categories.php">
+                                <div class="panel-footer">
+                                    <span class="pull-left">View Details</span>
+                                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                    <div class="clearfix"></div>
+                                </div>
+                            </a>
+                        </div>
                     </div>
                 </div>
                 <!-- /.row -->
 
-            </div>
-            <!-- /.container-fluid -->
+<?php 
+  $query= "SELECT * FROM posts WHERE post_status = 'draft'";
+  $select_all_draft_posts= mysqli_query($conn, $query);
+  $draft_post_counts= mysqli_num_rows($select_all_draft_posts);
+//   echo "<div class='huge'>{$draft_post_counts}</div>";
 
+  $query= "SELECT * FROM comments WHERE comment_status = 'reject'";
+  $select_all_rejected_comments= mysqli_query($conn, $query);
+  $rejeceted_comment_counts= mysqli_num_rows($select_all_rejected_comments);
+//   echo "<div class='huge'>{$draft_post_counts}</div>"
+
+    $query= "SELECT * FROM users WHERE user_role = 'subscriber'";
+    $select_all_user_subscriber= mysqli_query($conn, $query);
+    $subscriber_count= mysqli_num_rows($select_all_user_subscriber);
+    //   echo "<div class='huge'>{$draft_post_counts}</div>"
+?>
+                <div class="row">
+                <script type="text/javascript">
+                    google.charts.load('current', {'packages':['bar']});
+                    google.charts.setOnLoadCallback(drawChart);
+                    function drawChart() {
+                        var data = google.visualization.arrayToDataTable([
+                        ['Data', 'Count'],
+                        <?php 
+                            $element_text= ['Active Posts','Draft Posts', 'Comments', 'Pending Comments', 'Users', 'Subscribers', 'Categories'];
+                            $element_count= [$post_counts, $draft_post_counts, $comment_counts, $rejeceted_comment_counts, $user_counts, $subscriber_count, $categories_counts];
+
+                            for($i= 0; $i<count($element_count); $i++){
+                                if(!$element_count){
+                                    $element_count=0;
+                                }
+                                echo "['{$element_text[$i]}'" . " , " . "{$element_count[$i]}],";
+
+                            }
+                        ?>
+                        // ['Posts', 1030]
+                        ]);
+                        var options = {
+                        chart: {
+                            title: 'Company Performance',
+                            subtitle: 'Sales, Expenses, and Profit: 2014-2017',
+                        }
+                        };
+
+                        var chart = new google.charts.Bar(document.getElementById('columnchart_material'));
+
+                        chart.draw(data, google.charts.Bar.convertOptions(options));
+                    }
+                    </script>
+                </div>
+                <div id="columnchart_material" style="width: 'auto'; height: 500px;"></div>
+
+            <!-- </div> -->
+            
+            <!-- /.container-fluid -->
         </div>
+        
 
 
         <!-- /#page-wrapper -->
